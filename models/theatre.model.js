6fe536c775d319bf5c2659a0ef89ddc9fe5a27fe
Naman21-web <jainn0984@gmail.com>
2026-03-01 17:05:58 +0530

    Created model for Theatre

---
+++ models/theatre.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+/*
+    Schema defines theatre resource to be stored in the db.
+*/
+const theatreSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+    },
+    city:{
+        type: String,
+        required: true
+    },
+    pinCode:{
+        type: Number,
+        required: true
+    },
+    address: String,
+}, {timestamps: true});
+
+const Theatre = mongoose.model('Theatre', theatreSchema);
+
+module.exports = Theatre;
