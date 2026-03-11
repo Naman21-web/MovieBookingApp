f57502385424f33f4350d703af725f0dbd376c24
Naman21-web <jainn0984@gmail.com>
2026-03-11 19:55:52 +0530

    Feat: Added show model

---
+++ models/show.model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const showSchema = new mongoose.Schema({
+    theatreId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    movieId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    timing: {
+        type: String,
+        required: true
+    },
+    noOfSeats: {
+        type: Number,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    format: {
+        type: String
+    }
+}, {timestamps: true});
+
+const show = mongoose.model('Show',showSchema);
+module.exports = show;
